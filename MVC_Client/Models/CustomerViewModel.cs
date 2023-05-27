98296a157905b54c8b1b8c6afea226fca4b76c52
Nguyen Le Hung Phu <phunlh2001@gmail.com>
2023-05-27 21:58:29 +0700

    fix: enable nullable properties

--- MVC_Client/Models/CustomerViewModel.cs
+++ MVC_Client/Models/CustomerViewModel.cs
@@ -5,9 +5,9 @@
         public int Id { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
-        public string Fullname { get; set; }
-        public string Gender { get; set; }
-        public string Birthday { get; set; }
-        public string Address { get; set; }
+        public string? Fullname { get; set; }
+        public string? Gender { get; set; }
+        public string? Birthday { get; set; }
+        public string? Address { get; set; }
     }
 }
